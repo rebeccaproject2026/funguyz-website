b346fdc171d5378a4431bb757e49d816fb7b29b8
Bhavyata_Savaliya <bhavyta.savliya@tinyscript.in>
2026-06-17 18:41:53 +0530

    fix the www issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,32 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.funguyz.ca',
-          },
-        ],
-        destination: 'https://funguyz.ca/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.funguyz.com',
-          },
-        ],
-        destination: 'https://funguyz.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+
   async rewrites() {
     return [
       // Rewrite legacy /product/[slug] to /magic-mushrooms/[slug] as default fallback
